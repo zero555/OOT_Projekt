8a1756bc6653283bc1827adbf486df6a58cc7db3
domeix <domeix@web.de>
2014-05-22 11:49:36 +0200

    gemerget.

--- OOT_Projekt/src/KIgenerator.java
+++ OOT_Projekt/src/KIgenerator.java
@@ -1,2 +1,21 @@
 
+public class KIgenerator {
+    static String name = "Computer ";
+    static int nummer = 0;
+    
+    static String getKIName(){
+        nummer++;
+        return ""+name+nummer; 
+    }
+    
+    static Ecke getEcke(){
+		return null;
+        
+    }
+    
+    static String getSymbol(){
+		return name;
+        
+    }
+    
 }
