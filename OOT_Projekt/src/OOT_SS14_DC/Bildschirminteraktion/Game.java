df779d748a3a928961807a2b758b9e76a74f6cb7
domeix <domeix@web.de>
2014-05-23 16:50:37 +0200

    noch n bisschen weiter gemacht

--- OOT_Projekt/src/OOT_SS14_DC/Bildschirminteraktion/Game.java
+++ OOT_Projekt/src/OOT_SS14_DC/Bildschirminteraktion/Game.java
@@ -16,6 +16,8 @@ public class Game {
     
     private Scanner eingabe;
     
+    private int anzahlSpieler = 4;
+    
     /**
      * Game-Konstruktor,
      * es sollte jetzt die runGame()-Methode ausgefuert werden
@@ -36,13 +38,15 @@ public class Game {
      * @return      erstellter Spieler
      */
 	private Mensch erstelleSpieler(String name, Ecke ecke) {
-	    System.out.println("Bitte ein Symbol wählen.");
 	    String symbol = symbolWaehlen();
 	    Mensch spieler = new Mensch(name, ecke, symbol);
 		return spieler;
 	}
 
 	private Computer erstelleKi(String name, int schwierigkeitsgrad) {
+	    String symbol = symbolWaehlen();
+	    //Computer ki = KIgenerator(name, schwierigkeitsgrad);
+	    ///Simon, wie erzeuge ich eine KI??
 		return null;
 	}
 	
@@ -66,33 +70,56 @@ public class Game {
 	 * hier wird abgefragt, wer alles mitspielt
 	 * @return Gewinner des aktuellen Spiels
 	 */
-	public Spieler runGame() {
-		System.out.println("Wie viele Spieler?");
-		int spielerZahl = eingabe.nextInt();
-		
-		//Spielfeld erstellen
-		spielfeld = new Spielfeld(spielerZahl);
-		
-		System.out.println("menschlich?");
-		int mensch = eingabe.nextInt();
-		int menschenZaehler = 0;
-		
-		for (int i=0; i<spielerZahl; i++) {
-			System.out.println("Name fuer Spieler " + i );
-			String name = eingabe.next();
-			Ecke ecke = Ecke.A;	//muss geswitchcased werden
+	private void spielerErstellen() {
+	    System.out.println("Wie viele Spieler? (2-4)");
+	    int tmp = eingabe.nextInt();
+	    if(tmp<2) {
+	        tmp = 2;
+	    }
+	    if(tmp>4){
+	        tmp = 4;
+	    }
+        anzahlSpieler = tmp;
+        
+        //Spielfeld erstellen
+        spielfeld = new Spielfeld(anzahlSpieler);
+        
+        System.out.println("menschlich?");
+        int mensch = eingabe.nextInt();
+        int menschenZaehler = 0;
+        
+        for (int i=0; i<anzahlSpieler; i++) {
+            System.out.println("Name fuer Spieler " + i );
+            String name = eingabe.next();
+            Ecke ecke = Ecke.A; //muss geswitchcased werden
 
-			if (menschenZaehler < mensch) {
-				teilnehmer[i] = erstelleSpieler(name, ecke);
-				menschenZaehler++;
-			} else {
-				System.out.println("Schwierigkeit fuer Computerspieler waehlen.");
-				int schwierig = eingabe.nextInt();
-			teilnehmer[i] = erstelleKi(name, schwierig);
-			}
-		}
-		
-		
+            if (menschenZaehler < mensch) {
+                teilnehmer[i] = erstelleSpieler(name, ecke);
+                menschenZaehler++;
+            } else {
+                System.out.println("Schwierigkeit fuer Computerspieler waehlen.");
+                int schwierig = eingabe.nextInt();
+                teilnehmer[i] = erstelleKi(name, schwierig);
+            }
+        }
+        
+	}
+	private Spieler zufaelligerSpieler() {
+	    int zufall = (int)(Math.random()*anzahlSpieler);
+	    return teilnehmer[zufall];
+	}
+	
+	private void spielen() {
+	    aktuellerSpieler = zufaelligerSpieler();
+	    System.out.println(aktuellerSpieler);
+	    
+	    
+	    //gewinner muss zum Schluss gesetzt werden;
+	}
+	
+	public Spieler runGame() {
+		spielerErstellen();
+		spielen();
 		
 		//zum Schluss Scanner wieder zu machen.
 		eingabe.close();
